3d4a8e79969166d620cab893ef913a87cd39bf88
Lucas Wiener <lucas@wiener.se>
2013-04-03 16:33:53 +0200

    Added support for git dirty/clean prefix theme mods

--- lib/git.zsh
+++ lib/git.zsh
@@ -2,9 +2,23 @@
 function git_prompt_info() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || \
   ref=$(git rev-parse --short HEAD 2> /dev/null) || return
-  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+  echo "$ZSH_THEME_GIT_PROMPT_PREFIX$(parse_git_dirty_prefix)${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
 
+# Checks if working tree is dirty before branch is written
+parse_git_dirty_prefix() {
+  local SUBMODULE_SYNTAX=''
+  if [[ "$(git config --get oh-my-zsh.hide-status)" != "1" ]]; then
+    if [[ $POST_1_7_2_GIT -gt 0 ]]; then
+          SUBMODULE_SYNTAX="--ignore-submodules=dirty"
+    fi
+    if [[ -n $(git status -s ${SUBMODULE_SYNTAX}  2> /dev/null) ]]; then
+      echo "$ZSH_THEME_GIT_PROMPT_DIRTY_PREFIX"
+    else
+      echo "$ZSH_THEME_GIT_PROMPT_CLEAN_PREFIX"
+    fi
+  fi
+}
 
 # Checks if working tree is dirty
 parse_git_dirty() {
