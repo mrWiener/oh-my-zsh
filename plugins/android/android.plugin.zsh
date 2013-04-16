17ff90ac0bec96eab326e81bea93ff7655b56ed9
Lucas Wiener <lucas@wiener.se>
2013-04-16 10:33:02 +0200

    Added android plugin with aliases for adb

---
+++ plugins/android/android.plugin.zsh
@@ -0,0 +1,5 @@
+#TODO: prompt user with android sdk installation path if not present
+
+#aliases
+alias adbc='$ANDROID_SDK_PATH/platform-tools/adb connect'
+alias adbr='$ANDROID_SDK_PATH/platform-tools/adb kill-server; $ANDROID_SDK_PATH/platform-tools/adb start-server'
