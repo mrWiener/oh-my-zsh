ca77540a74abbbb3bb0eaf1569119f335d1c2384
Lucas Wiener <lucas@wiener.se>
2013-04-22 10:47:34 +0200

    Added path export to android plugin

--- plugins/android/android.plugin.zsh
+++ plugins/android/android.plugin.zsh
@@ -1,4 +1,5 @@
-#TODO: prompt user with android sdk installation path if not present
+#TODO: prompt user with android sdk installation path if not present and set it like below.
+export ANDROID_SDK_PATH=/Applications/android-dev/sdk/
 
 #aliases
 alias adbc='$ANDROID_SDK_PATH/platform-tools/adb connect'
